ff3196cb379a711bbb94026b263261fee578c7a3
HirokiSatoh <sh.0921.soccer@outlook.jp>
2020-06-21 16:29:46 +0900

    photoモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_21_063045) do
+ActiveRecord::Schema.define(version: 2020_06_21_071348) do
 
   create_table "blogs", force: :cascade do |t|
     t.string "title", null: false
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 2020_06_21_063045) do
     t.integer "user_id", null: false
     t.integer "category_id", null: false
     t.string "image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "image_id"
+    t.string "title"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
