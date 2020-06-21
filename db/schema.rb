81a7083e916d544025ce1da97dfe560498ee2583
HirokiSatoh <sh.0921.soccer@outlook.jp>
2020-06-21 17:23:42 +0900

    categoryモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_21_081429) do
+ActiveRecord::Schema.define(version: 2020_06_21_082043) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -30,6 +30,13 @@ ActiveRecord::Schema.define(version: 2020_06_21_081429) do
     t.integer "user_id", null: false
     t.integer "category_id", null: false
     t.string "image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name", null: false
+    t.boolean "is_valid", default: true, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
