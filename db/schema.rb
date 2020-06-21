5d95a0e5fae3e61a0ac6ccf70417ac8eadb25537
HirokiSatoh <sh.0921.soccer@outlook.jp>
2020-06-21 20:05:24 +0900

    blog_comment モデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_21_082043) do
+ActiveRecord::Schema.define(version: 2020_06_21_110134) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +22,16 @@ ActiveRecord::Schema.define(version: 2020_06_21_082043) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "blog_comments", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "blog_id"
+    t.string "comment"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["blog_id"], name: "index_blog_comments_on_blog_id"
+    t.index ["user_id"], name: "index_blog_comments_on_user_id"
   end
 
   create_table "blogs", force: :cascade do |t|
