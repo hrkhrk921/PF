72211b4b4f5e564a9094df8f4f142c2340ca8be9
HirokiSatoh <sh.0921.soccer@outlook.jp>
2020-06-21 16:33:18 +0900

    photoモデル編集

--- db/migrate/20200621071348_create_photos.rb
+++ db/migrate/20200621071348_create_photos.rb
@@ -1,9 +1,9 @@
 class CreatePhotos < ActiveRecord::Migration[5.2]
   def change
     create_table :photos do |t|
-    	t.integer :user_id
-    	t.string :image_id
-    	t.string :title
+    	t.integer :user_id, null: false
+    	t.string :image_id, null: false
+    	t.string :title, null: false
     	t.text :body
 
       t.timestamps
