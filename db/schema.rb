1503a5855958945571ecd82053114aab04b57b94
HirokiSatoh <sh.0921.soccer@outlook.jp>
2020-06-21 20:31:07 +0900

    relationshipモデル作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_21_112349) do
+ActiveRecord::Schema.define(version: 2020_06_21_112915) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -85,6 +85,11 @@ ActiveRecord::Schema.define(version: 2020_06_21_112349) do
     t.string "image_id"
     t.string "title"
     t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "relationships", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
