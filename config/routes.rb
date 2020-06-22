0e1f3689412d784b770304a80108b1c981bd95f5
HirokiSatoh <sh.0921.soccer@outlook.jp>
2020-06-22 17:58:40 +0900

    blogページ作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,21 @@
 Rails.application.routes.draw do
+  root to: 'home#top'
+  get '/about'=>'home#about'
 
-	#devise 会員
-	devise_for :users, controllers: {
-		sessions:      'user_devises/sessions',
-		passwords:     'user_devises/passwords',
-		registrations: 'user_devises/registrations'
-	}
-	#devise 管理者
-	devise_for :admins, controllers: {
-		sessions:      'admin_devises/sessions',
-		passwords:     'admin_devises/passwords',
-		registrations: 'admin_devises/registrations'
-	}
+  #devise 会員
+  devise_for :users, controllers: {
+    sessions:      'user_devises/sessions',
+    passwords:     'user_devises/passwords',
+    registrations: 'user_devises/registrations'
+  }
+  #devise 管理者
+  devise_for :admins, controllers: {
+    sessions:      'admin_devises/sessions',
+    passwords:     'admin_devises/passwords',
+    registrations: 'admin_devises/registrations'
+  }
+
+  #blogのルート
+  resources :blogs, only: [:new, :index, :show, :edit, :create, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
