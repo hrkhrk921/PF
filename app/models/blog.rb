3c6b5a4c40a8e083982cfcd6ed8a9ec8e2fd1ba3
HirokiSatoh <sh.0921.soccer@outlook.jp>
2020-07-12 20:20:20 +0900

    destroy修正

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,7 +2,7 @@ class Blog < ApplicationRecord
   attachment :image
   belongs_to :category,optional: true
   belongs_to :user
-  has_many :blog_comments
+  has_many :blog_comments, dependent: :destroy
   validates :title, presence: { message: 'タイトルを入力してください' }
   validates :category, presence: { message: 'カテゴリーを選択してください' }
   validates :body, presence: { message: '本文を入力してください' }
