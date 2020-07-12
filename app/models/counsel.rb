ed30c13acf5dc0476755d02756497fb800e32101
HirokiSatoh <sh.0921.soccer@outlook.jp>
2020-07-12 15:23:50 +0900

    投稿削除

--- app/models/counsel.rb
+++ app/models/counsel.rb
@@ -1,7 +1,7 @@
 class Counsel < ApplicationRecord
   attachment :image
   belongs_to :user
-  has_many :counsel_comments
+  has_many :counsel_comments, dependent: :destroy
   validates :title, presence: { message: 'タイトルを入力してください' }
   validates :body, presence: { message: '本文を入力してください' }
 end
