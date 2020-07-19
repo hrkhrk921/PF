e7757e0ee4afdf9a43165f2211be80c90122d4e0
HirokiSatoh <sh.0921.soccer@outlook.jp>
2020-07-19 14:35:56 +0900

    validation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_02_070906) do
+ActiveRecord::Schema.define(version: 2020_07_17_152921) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -47,6 +47,13 @@ ActiveRecord::Schema.define(version: 2020_07_02_070906) do
   create_table "categories", force: :cascade do |t|
     t.string "name", null: false
     t.boolean "is_valid", default: true, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "email"
+    t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
