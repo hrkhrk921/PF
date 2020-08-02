2e8f77afbe2ff1f5c65cf5b5235eeb404a8a4f61
HirokiSatoh <sh.0921.soccer@outlook.jp>
2020-08-02 17:19:11 +0900

    admin/home

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -1,6 +1,6 @@
 class Admin::HomeController < ApplicationController
   before_action :authenticate_admin!
-  def top
-    
+  def top #今日の日付取得
+	@contacts = Contact.where(created_at: 0.day.ago.all_day)
   end
 end
