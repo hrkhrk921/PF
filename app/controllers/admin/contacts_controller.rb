f10598ebb53b388d79053db67a1ebca73cf490c3
HirokiSatoh <sh.0921.soccer@outlook.jp>
2020-08-05 06:39:04 +0900

    修正

--- app/controllers/admin/contacts_controller.rb
+++ app/controllers/admin/contacts_controller.rb
@@ -1,8 +1,8 @@
 class Admin::ContactsController < ApplicationController
   def index
-	if params[:order_views] == "admin/home"
-		@contacts = Contact.where(created_at: 0.day.ago.all_day)
-	else
+  	if params[:order_views] == "admin/home"
+  		@contacts = Contact.where(created_at: 0.day.ago.all_day)
+  	else
 	    @contacts = Contact.all
     end
   end
