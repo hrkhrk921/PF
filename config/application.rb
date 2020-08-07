dec15c1ba0509228a6f400481325712e3c64286c
HirokiSatoh <sh.0921.soccer@outlook.jp>
2020-08-07 22:11:40 +0900

    多言語化yml

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,15 @@ module PF
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+
+    config.i18n.available_locales = %i(ja en)
+
+    config.i18n.enforce_available_locales = true
+
+    config.i18n.default_locale = :ja
+
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
