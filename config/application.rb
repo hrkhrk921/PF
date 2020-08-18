dabe506cfd911b689b0bef941f2ac7ac313e52d4
HirokiSatoh <sh.0921.soccer@outlook.jp>
2020-08-18 16:04:39 +0900

    config/ja

--- config/application.rb
+++ config/application.rb
@@ -10,13 +10,7 @@ module PF
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
-
-    config.i18n.available_locales = %i(ja en)
-
-    config.i18n.enforce_available_locales = true
-
+    config.i18n.load_path += Dir[Rails.root.join('config','locales','*.{rb,yml}').to_s]
     config.i18n.default_locale = :ja
 
 
