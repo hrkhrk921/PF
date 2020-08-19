62da3ab401b1946af3c1639ed1f895c8ede65f8c
HirokiSatoh <sh.0921.soccer@outlook.jp>
2020-08-19 16:28:52 +0900

    rspec/user

--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -11,4 +11,15 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe UsersHelper, type: :helper do
+  it "名前とメールアドレスとパスワードがあれば登録できる"
+
+  it "名前がなければ登録できない"
+
+  it "メールアドレスがなければ登録できない"
+
+  it "メールアドレスが重複していたら登録できない"
+
+  it "パスワードがなければ登録できない"
+
+  it "パスワードが暗号化されているか"
 end
