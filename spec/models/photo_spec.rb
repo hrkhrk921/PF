5f1c81d1b308a21fa0ad63331cb6b30d0f0c000f
HirokiSatoh <sh.0921.soccer@outlook.jp>
2020-08-23 00:05:22 +0900

    photo/rspec

--- spec/models/photo_spec.rb
+++ spec/models/photo_spec.rb
@@ -1,4 +1,17 @@
 require 'rails_helper'
 
-RSpec.describe Photo, type: :model do
+RSpec.describe Photo,"モデルに関するテスト",type: :model do
+  context "データが正しく保存される" do
+    before do
+      @photo = Photo.new
+      @photo.title = "Hello WebCamp"
+      @phto.body = "今日も晴れです。"
+      @photo.user_id = "1"
+      @photo.image_id = "1"
+      @photo.save
+    end
+    it "全て入力してあるので保存される" do
+      expect(@photo).to be_valid
+    end
+  end
 end
