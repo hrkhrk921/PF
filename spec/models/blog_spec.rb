0f7314fd8a4893209c4dea5857742f217759803a
HirokiSatoh <sh.0921.soccer@outlook.jp>
2020-08-22 10:48:07 +0900

    rspec/blog

--- spec/models/blog_spec.rb
+++ spec/models/blog_spec.rb
@@ -15,4 +15,28 @@ RSpec.describe Blog, "モデルに関するテスト", type: :model do
           expect(@blog).to be_valid
       end
   end
+  describe 'バリデーションのテスト' do
+    let(:user) { create(:user) }
+    let!(:blog) { build(:blog, user_id: user.id) }
+
+    context 'titleカラム' do
+      it '空欄でないこと' do
+        blog.title = ''
+        expect(blog.valid?).to eq false;
+      end
+    end
+    context 'bodyカラム' do
+      it '空欄でないこと' do
+        blog.body = ''
+        expect(blog.valid?).to eq false;
+      end
+    end
+  end
+  describe 'アソシエーションのテスト' do
+    context 'Userモデルとの関係' do
+      it 'N:1となっている' do
+        expect(Blog.reflect_on_association(:user).macro).to eq :belongs_to
+      end
+    end
+  end
 end
