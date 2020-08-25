4c8ef997a1501d0fb4d5005fe38803d2a54fac71
HirokiSatoh <sh.0921.soccer@outlook.jp>
2020-08-25 20:34:15 +0900

    counsel

--- spec/models/counsel_spec.rb
+++ spec/models/counsel_spec.rb
@@ -1,4 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Counsel, type: :model do
+  context "データが正しく保存される" do
+    before do
+      @counsel = Counsel.new
+      @counsel.title = "Hello WebCamp"
+      @counsel.body = "今日も晴れです。"
+      @counsel.user_id = "1"
+      @counsel.image_id = "1"
+      @counsel.save
+    end
+    it "全て入力してあるので保存される" do
+      expect(@counsel).to be_valid
+    end
+  end
 end
