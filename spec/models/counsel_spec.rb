af32eede45eb5bb41aabc8c7cc6e45091f976247
HirokiSatoh <sh.0921.soccer@outlook.jp>
2020-08-26 23:08:46 +0900

    counsel/rspec

--- spec/models/counsel_spec.rb
+++ spec/models/counsel_spec.rb
@@ -14,4 +14,28 @@ RSpec.describe Counsel, type: :model do
       expect(@counsel).to be_valid
     end
   end
+  describe 'バリデーションのテスト' do
+    let(:user) { create(:user) }
+    let!(:counsel) { build(:counsel, user_id: user.id) }
+
+    context 'titleカラム' do
+      it '空欄でないこと' do
+        counsel.title = ''
+        expect(counsel.valid?).to eq false;
+      end
+    end
+    context 'bodyカラム' do
+      it '空欄でないこと' do
+        counsel.body = ''
+        expect(counsel.valid?).to eq false;
+      end
+    end
+  end
+  describe 'アソシエーションのテスト' do
+    context 'Userモデルとの関係' do
+      it 'N:1となっている' do
+        expect(Counsel.reflect_on_association(:user).macro).to eq :belongs_to
+      end
+    end
+  end
 end
