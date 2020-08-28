718435e741c61bbc1a0078186d6db32a93d283d7
HirokiSatoh <sh.0921.soccer@outlook.jp>
2020-08-28 13:52:54 +0900

    rspec

--- spec/models/counsel_comment_spec.rb
+++ spec/models/counsel_comment_spec.rb
@@ -1,4 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe CounselComment, type: :model do
+	before do
+    user = FactoryBot.create(:user)
+    second_user = FactoryBot.create(:second_user)
+    stroll = FactoryBot.create(:stroll, user: second_user)
+    visit root_path
+    click_link 'Login'
+    fill_in('user_email', with: 'user@com')
+    fill_in('user_password', with: 'password')
+    click_on 'ログイン'
+    click_link 'Strolls'
+    click_link ('記事を読む'), match: :first
+  end
 end
