9b51cd091502285c3fed75a390e38e816e9c1e5d
HirokiSatoh <sh.0921.soccer@outlook.jp>
2020-09-01 20:19:36 +0900

    rspec

--- spec/models/counsel_comment_spec.rb
+++ spec/models/counsel_comment_spec.rb
@@ -13,4 +13,36 @@ RSpec.describe CounselComment, type: :model do
     click_link 'Strolls'
     click_link ('記事を読む'), match: :first
   end
+  describe '投稿詳細画面' do
+    context "ログインしているユーザーが投稿者以外の場合" do
+      it 'コメントを投稿できること' do
+        fill_in('comment_content', with: 'chiba')
+        click_on 'コメントする'
+        expect(page).to have_content 'chiba'
+      end
+      it '自分のコメントを編集できること' do
+        fill_in('comment_content', with: 'chiba')
+        click_on 'コメントする'
+        click_link '編集'
+        fill_in('edit_comment', with: 'chibachiba')
+        click_on '編集する'
+        expect(page).to have_content 'chibachiba'
+      end
+      it '自分のコメントを削除できること' do
+        fill_in('comment_content', with: 'chiba')
+        click_on 'コメントする'
+        click_link '削除'
+        expect(page).to have_no_content 'chiba'
+      end
+    end
+    context "ユーザーがログアウトしている場合" do
+      it 'コメントを投稿できないこと' do
+        click_link 'Logout'
+        click_link 'Strolls'
+        click_link ('記事を読む'), match: :first
+        expect(page).to have_no_button 'コメントする'
+      end
+    end
+  end
+end
 end
